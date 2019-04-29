bc60c87a383be6a5b20daeb2a4908a9265e53892
Jaccojwang <danolago@gmail.com>
2019-04-29 17:29:11 +0300

    Create new migrations and chand db name

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://stephenotieno:ijacco@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://stephenotieno:ijacco@localhost/blogger'
     DEBUG = True
 
 config_options = {
--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 from app import create_app,db, admin
-from app.models import User
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Post, Comment
